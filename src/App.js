d3c837370ccabac44c58d678a6acd1e79a4c7391
David Ballerini <david.ballerini@extrasys.it>
2021-03-22 16:14:34 +0100

    prova di cambiamento su branch diverso

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          Son molto stanco
+          Cambiato in dev branch
         </p>
       </div>
     );
