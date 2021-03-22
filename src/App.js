f9cab97758282e3fe59c4c2537bb6b8165962d68
David Ballerini <david.ballerini@extrasys.it>
2021-03-22 16:46:23 +0100

    cambiamento fatto apposta per causare conflitto di merge

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          Cambiato in dev branch
+          Cambiato in master branch per causare conflitto
         </p>
       </div>
     );
